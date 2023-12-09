2a3dfbe27c4748a4b88679b6125134b5417aa518
Lucky Jain <luckyjain78281@gmail.com>
2023-12-10 04:00:09 +0530

    getting started with abstract pattern

---
+++ System Design/Creational/AbstractFactory.ts
@@ -0,0 +1,9 @@
+/*
+The Abstract Factory Pattern usually defines the interfaces of a collection of factory
+methods, without specifying concrete products. This allows an entire factory to be
+replaceable, in order to produce different products following the same production outline.
+
+Different from the Factory Method Pattern, the Abstract Factory Pattern extracts another
+part called client that take cares of shaping the outline of the building process. This makes
+the factory part focused more on producing each component.
+*/
