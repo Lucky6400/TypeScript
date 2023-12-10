9f23242c3bcb6fc27ef711a316d73e1c5388a9b0
Lucky Jain <luckyjain78281@gmail.com>
2023-12-10 23:18:32 +0530

    getting started with builder pattern

---
+++ System Design/Creational/BuilderPattern.ts
@@ -0,0 +1,6 @@
+/*
+Builder Pattern encapsulates components by exposing only the building steps and
+provides the final products directly. At the same time, the Builder Pattern also encapsulates
+the internal structures of a product. This makes it possible for a more flexible abstraction
+and implementation of building complex objects.
+*/
