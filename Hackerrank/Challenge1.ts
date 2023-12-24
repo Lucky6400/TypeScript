de235c753dc49b5dec87ac026d0236b8d09f095f
Lucky Jain <luckyjain78281@gmail.com>
2023-12-24 12:19:41 +0530

    added question

--- Hackerrank/Challenge1.ts
+++ Hackerrank/Challenge1.ts
@@ -1,5 +1,25 @@
 'use strict';
 
+/*
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+
+
+Return .
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of a and b
+*/
+
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 let inputString: string = '';
@@ -29,5 +49,4 @@ function main() {
     }
 
     solveMeFirst(a, b);
-
 }
