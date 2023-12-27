b6ca1b2b92f36af92304ecbad1303eb4ad062af8
Lucky Jain <luckyjain78281@gmail.com>
2023-12-28 02:26:55 +0530

    prototype pattern

--- System Design/Creational/PrototypePattern.ts
+++ System Design/Creational/PrototypePattern.ts
@@ -1,21 +1,91 @@
 /*
-Sometimes we may need to add a subclass just for changing the class name while
-performing the same new operation. Instances of a single class usually share the same
-methods and properties, so we can clone one existing instance for new instances to be
-created. That is the concept of a prototype.
+A Prototype is a kind of object that takes its initial state and properties out of existing
+objects. The main idea is to avoid having to manually create an object and assign
+properties to it from another object.
 */
 
-/* This pattern has no image in book */
+const person = {
+    name: "",
+    age: 0,
+    gender: "",
+    greet: function () {
+        console.log("Hello, I'm " + this.name);
+    },
+    introduce: function () {
+        console.log("My name is " + this.name + ", I'm " + this.age + " years old, and I'm " + this.gender);
+    }
+};
 
-class Base {
-    state: number;
+const alice = Object.create(person); // create a new object using the person prototype
+alice.name = "Alice"; // assign some values to the properties
+alice.age = 25;
+alice.gender = "female";
+
+
+
+// implementation of Prototype pattern
+
+interface CricketerPrototype {
+    clone(): CricketerPrototype;
+    introduce(): void;
 }
 
-let base = new Base();
-base.state = 0;
-class Derived extends Base { }
-Derived.prototype = base;
-let derived = new Derived();
+class AjitAgarkar implements CricketerPrototype {
+    private bowlingSpeed: number;
+    private ratingBowl: number;
+    private ratingBat: number;
+    constructor(bowlingSpeed: number, ratingBowl: number, ratingBat: number) {
+        this.bowlingSpeed = bowlingSpeed;
+        this.ratingBat = ratingBat;
+        this.ratingBowl = ratingBowl;
+    }
+    clone(): AjitAgarkar {
+        const cloned = Object.create(AjitAgarkar.prototype || null);
+        Object.getOwnPropertyNames(this).map((key: string) => {
+            cloned[key] = this[key];
+        })
+        return cloned
+    }
+
+    public introduce(): void {
+        console.log(`
+        Ajit Agarkar: India Cricketer
+        Bowling Speed: ${this.bowlingSpeed}
+        Batting Rating: ${this.ratingBat}
+        Bowling Rating: ${this.ratingBowl}
+        `)
+    }
+}
+
+class RobinUtthappa implements CricketerPrototype {
+    private strikeRate: number;
+    private ratingBat: number;
+    constructor(strikeRate: number, ratingBat: number) {
+        this.strikeRate = strikeRate;
+        this.ratingBat = ratingBat;
+    }
+    clone(): RobinUtthappa {
+        const cloned = Object.create(RobinUtthappa.prototype || null);
+        Object.getOwnPropertyNames(this).map((key: string) => {
+            cloned[key] = this[key];
+        })
+        return cloned
+    }
+
+    public introduce(): void {
+        console.log(`
+        Robin Utthappa: India Cricketer
+        Strike Rate: ${this.strikeRate}
+        Batting Rating: ${this.ratingBat}
+        `)
+    }
+}
+
+const ajitA: CricketerPrototype = new AjitAgarkar(145, 91, 75);
+const robinU: CricketerPrototype = new RobinUtthappa(130, 85);
+
+ajitA.clone().introduce();
+robinU.clone().introduce();
 
 /*
 Now, the derived object will keep the state of the base object. This could be useful when
