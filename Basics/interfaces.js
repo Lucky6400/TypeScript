884753d42de5ab5b925fb22a943bf7fc450058b2
Lucky Jain <luckyjain78281@gmail.com>
2024-01-06 13:40:55 +0530

    js deletion

--- Basics/interfaces.js
+++
@@ -1,8 +0,0 @@
-//Interfaces are similar to type aliases, except they only apply to object types.
-var chelsea = {
-    players: 11,
-    captain: "Cesar Azpiliqueta",
-    manager: "Graham Potter",
-    champions: false
-};
-console.log(chelsea);
--- Basics/ts-arrays.js
+++
@@ -1,7 +0,0 @@
-var players = ["Mikel", "Terry", "Willian", "Hazard", "Cech", "Drogba"];
-players.push("Diego Costa");
-//players.push(3) // Argument of type 'number' is not assignable to parameter of type 'string'.
-// inference
-var runs = [22, 33, 44, 55, 66];
-runs.push(100);
-runs.push("No run");
