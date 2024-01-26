8f6c21d6286b89460a82e1c3e0ebce70a6e05727
Lucky Jain <luckyjain78281@gmail.com>
2024-01-27 00:02:04 +0530

    modern implementation of ts decorators

--- System Design/Structural/Decorator/Decorator.ts
+++ System Design/Structural/Decorator/Decorator.ts
@@ -15,7 +15,7 @@ export class BatsmanDecorator implements Cricketer {
 
     // we wanted to add a check for runs but we did not want to modify existing class, hence we used this decorator.
     public groundedShot(runs: number): void {
-        if(runs > 4) return undefined;
+        if (runs > 4) return undefined;
         this.cricketer.groundedShot(runs);
     }
 }
@@ -25,3 +25,36 @@ const batDec = new BatsmanDecorator(batsman);
 batDec.groundedShot(4);
 batDec.groundedShot(6);
 batDec.groundedShot(2);
+
+
+
+
+/* Modern Implementation */
+
+function Decorator() {
+    return function (
+        target: Object,
+        key: string | symbol,
+        descriptor: PropertyDescriptor
+    ) {
+        const caller = descriptor.value;
+        descriptor.value = (runs: number) => {
+            if(runs > 4) return undefined;
+            caller.apply(this, [runs]);
+            return caller;
+        };
+        return descriptor;
+    };
+}
+
+class Sehwag {
+    @Decorator()
+    groundedShot(runs: number) {
+        runs === 4 ? console.log("A powerful shot for four by Sehwag.") : console.log("Sehwag, just pushes it for " + runs + " runs.");
+    }
+}
+
+const sehwag = new Sehwag();
+sehwag.groundedShot(3);
+sehwag.groundedShot(4);
+sehwag.groundedShot(6);
