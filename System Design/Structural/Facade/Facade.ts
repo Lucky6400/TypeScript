de57182288a44543b14fee78e02dffcb5451ab10
Lucky Jain <luckyjain78281@gmail.com>
2024-01-27 23:45:46 +0530

    started learning facade

---
+++ System Design/Structural/Facade/Facade.ts
@@ -0,0 +1,20 @@
+/*
+Façade is a pattern that also wraps one or more interfaces and hides the complexities of
+using complex workflows under a simpler interface.
+
+When you have some workflows that need to be orchestrated in a specific manner,
+such as calling one service and then the other under certain criteria, it's quite tricky
+to bring this logic across your components every time. With this pattern, you hide all
+those complexities behind an API and offer a simpler, more readable way to call those
+workflows. In simple terms, you use a function to wrap many service calls together so that
+the client will call it with fewer parameters.
+
+One analogy of this pattern is having a smartphone, and you have to type the number you
+want to call. Instead of calling the number by typing it, you use quick actions to obtain
+a number from a list and you call the number when you tap on the quick action button.
+Although you can still manually enter the numbers, the UI Façade can carry out this
+process for you in one go. Let's explore when you will have to use this pattern in practice.
+*/
+
+
+
